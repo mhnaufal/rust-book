f7879bc3bc3d50d22f200a6311fd3ab76064fe58
mhnaufal <aal.pirates16@gmail.com>
2021-08-12 10:58:06 +0700

    Function

---
+++ src/function.rs
@@ -0,0 +1,28 @@
+/* 
+* Rust doesn't care where we put the invoked function
+* as long as we made it
+*/
+pub fn main_function() {
+    parameter_function(313);
+
+    /* Statement & Expression*/
+    let x = 1;
+    // let a = let b = 2;  // ERROR: because statement doesn't return a value so we can't use this
+
+    let y = {
+        x + 5
+    };
+    println!("Statement with expression: x = {}, y = {:?}", x, y);
+
+    let _return_function = return_function(1);
+    println!("Function with return: {}", _return_function);
+}
+
+/* The type of parameter in function must be declared specifically */
+pub fn parameter_function(num: i32) {
+    println!("The value of parameter inside parameter_function is: {}", num);
+}
+
+fn return_function(num: i32) -> i32 {
+    num + 10
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+#![allow(dead_code)]
+
 mod variable;
 mod data_type;
+mod function;
 
 fn main() {
     // variable::variable();
-    data_type::data_type();
+    // data_type::data_type();
+    function::main_function();
 }
