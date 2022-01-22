3782e49499acefc434d7103e15c9ecca30d612b7
mhnaufal <aal.pirates16@gmail.com>
2022-01-22 12:34:30 +0700

    update main

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,19 @@ mod borrowing;
 mod r#struct;
 mod r#enum;
 mod vectors;
+mod challenges;
+use challenges::armstrong;
 
 fn main() {
-    // variable::variable();
-    // data_type::data_type();
-    // function::main_function();
-    // control_flow::control_flow();
-    // ownership::ownership();
-    // borrowing::borrow();
-    // r#struct::r#struct();
-    // r#enum::r#enum();
+    variable::variable();
+    data_type::data_type();
+    function::main_function();
+    control_flow::control_flow();
+    ownership::ownership();
+    borrowing::borrow();
+    r#struct::r#struct();
+    r#enum::r#enum();
     vectors::vector();
+    challenges::hello_from_challenges();
+    armstrong::is_armstrong_number(1);
 }
