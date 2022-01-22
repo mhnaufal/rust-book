d5607c399838bd23a54bf5eb82606b888306f04f
mhnaufal <aal.pirates16@gmail.com>
2022-01-22 12:34:09 +0700

    initiate challenges folder

---
+++ src/challenges/armstrong.rs
@@ -0,0 +1,6 @@
+#![allow(dead_code)]
+
+pub fn is_armstrong_number(num: u32) -> bool {
+    println!("{:?}", num);
+    true
+}
---
+++ src/challenges/mod.rs
@@ -0,0 +1,8 @@
+/* We can create the module inside this file
+ * or we can also import a module from another file within this folder.
+ */
+pub mod armstrong;
+
+pub fn hello_from_challenges() {
+    println!("Hello from challenges folder!");
+}
