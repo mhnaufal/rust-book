ebe04f57af9bf0cf60cb6d2786c774a303cd6ca0
mhnaufal <aal.pirates16@gmail.com>
2022-01-22 10:40:39 +0700

    update variables

--- src/variable.rs
+++ src/variable.rs
@@ -1,26 +1,58 @@
 #![allow(dead_code)]
 
-pub fn variable () {
-    // Variables are immutable by default
-    let nim = 240601;
-    println!("Your NIM is: {}", &nim);
+pub fn variable() {
+    /* Imutable variable
+     * Variables are immutable by default
+     */
+    let nim = 240601191;
+    println!("Your current NIM is: {}", &nim);
 
     // ERROR - because reassign immutable variable
     // nim = 240602;
 
-    // Not cause error because it's a 'shadowing' technique 
-    // Create a new variable with same name but different type
+    /* Shadowing
+     * Not cause error because it's a 'shadowing' technique
+     * Create a 'new variable' with 'same name' but 'different data type'
+     */
     let nim = 240602.1;
     println!("Your new NIM is: {}", &nim);
 
-    let mut gpa: f32 = 3.8;
-    println!("Your GPA is: {}", gpa);
+    println!("----------------------------------------------------");
 
-    gpa = 4.0;
+    /* However, how do we get the first nim variable? */
+    /* The answer is by using block */
+    let weight = 80;
+    println!("Your weight is: {}", weight); // will result 80
+    {
+        let weight = 80.5;
+        println!("Your weight is: {}", weight); // will result 80.5
+    }
+    println!("Your weight is: {}", weight); // will result 80
+
+    println!("----------------------------------------------------");
+
+    /* Casting data type */
+    let mut gpa: f32 = 3.8;
+    println!("Your current GPA is: {}", gpa);
+
+    gpa = (gpa as i64) as f32;
     println!("Your new GPA is: {}", &gpa);
 
-    // const are always immutable
-    // and we need to define the type manually
+    /* Constant
+     * const are always immutable
+     * and we need to define the type manually
+     */
     const MAX_GPA: f64 = 4.0;
     println!("Maximum GPA is: {}", MAX_GPA);
+
+    /* Variable expression */
+    let calculate = {
+        let first_number = 1;
+        let second_number = 2;
+        let first_number = first_number + 3;
+        let first_number = first_number + second_number;
+        first_number
+    };
+
+    println!("The result = {}", calculate);
 }
