5a7f2fbf7ab19f7ca7e4ba632eedf91409278dbe
mhnaufal <aal.pirates16@gmail.com>
2022-02-19 09:05:44 +0700

    unrecoverable error

---
+++ src/errors.rs
@@ -0,0 +1,50 @@
+#![allow(dead_code)]
+
+use std::{fs::File, io::ErrorKind, panic};
+
+/// 2 types of Error in Rust
+/// Recoverable & Unrecoverable
+/// Recoverable = User error e.g. missing file -- Result<T, E>>
+/// Unrecoveralbe = System error e.g. array out of bound -- panic!
+
+pub fn errors() {
+    /* Unrecoverable Error (panic!) */
+
+    // panic!("Manual panic error");
+
+    // RUST_BACKTRACE=1 cargo run
+    // to get 'debug' like data from panic
+
+    /* Enum Result */
+    let f = File::open("src/mom.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => panic!("Error while opening the file: {:?}", error),
+    };
+
+    println!("File = {:?}", f);
+
+    let f = File::open("src/mom.txt");
+
+    let g = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("src/mom.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem while creating a file: {:?}", e),
+            },
+            other_error => {
+                panic!("Problem while opening the file: {:?}", other_error)
+            }
+        },
+    };
+
+    println!("File 2 = {:?}", g);
+
+    /* unwrap & expect */
+    let h = File::open("src/mom.txt").unwrap();
+    let _h = File::open("src/mom.txt").expect("Error 3 while opening the mom.txt");
+
+    println!("File 3 = {:?}", h);
+}
