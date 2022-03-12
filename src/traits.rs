9bed3d1f502e4d5285a4ae094a08fc2d540da229
mhnaufal <aal.pirates16@gmail.com>
2022-03-12 17:46:35 +0700

    add trait as parameter of a function

--- src/traits.rs
+++ src/traits.rs
@@ -4,6 +4,7 @@
 * scope trait trait_name
 * there is no implementation body in the trait
 * only declaration
+* looks very similiar with "interface"
 */
 pub trait Summary {
     fn summarize(&self) -> String;
@@ -39,6 +40,13 @@ impl Summary for Tweet {
     }
 }
 
+/* Trait as parameter
+* take a trait of type 'Summary' as parameter
+*/
+pub fn notification(_media: &impl Summary) -> String {
+    format!("🌐 Notify")
+}
+
 pub fn traits() {
     let my_tweet = Tweet {
         username: String::from("ikiuyuu"),
@@ -50,4 +58,13 @@ pub fn traits() {
 
     println!("There is a new tweet!: {}", my_tweet.summarize());
     println!("🔼 tweet by {}", my_tweet.check_author());
+
+    println!("+----------------------------+");
+
+    println!("Tweet notification: {}", notification(&my_tweet));
+
+    // Will rise error because notification function takes a type of Summary trait
+    // but we give it a type of int32
+    // let tmp = 313;
+    // println!("Tweet notification: {}", notification(&tmp));
 }
