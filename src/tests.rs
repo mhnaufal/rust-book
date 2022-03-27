cd9915920b73a5ee42f9ee3c801052061de00aac
mhnaufal <aal.pirates16@gmail.com>
2022-03-28 05:57:56 +0700

    update assert test

--- src/tests.rs
+++ src/tests.rs
@@ -10,3 +10,34 @@ mod test {
         assert_ne!(2 + 1, 4);
     }
 }
+
+/* Assert macro
+* if an argument evaluate to true, the test passes, else it throws panic!
+*/
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn is_can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+#[test]
+fn larger_can_hold_smaller() {
+    let larger = Rectangle {
+        width: 9,
+        height: 10,
+    };
+
+    let smaller = Rectangle {
+        width: 6,
+        height: 4,
+    };
+
+    assert!(larger.is_can_hold(&smaller));
+}
